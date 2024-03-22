43fafe1737642423b8de963f2c73418ede62a671
Svetlana Novikova <seaciliya2012@yandex.ru>
2024-03-22 15:32:07 +0300

    '2'

--- DBManager.py
+++ DBManager.py
@@ -4,4 +4,22 @@ class DBManager():
         pass
 
     def get_companies_and_vacancies_count(self):
+        """
+        Получает список всех компаний и количество вакансий у каждой компании
+        :return:
+        """
         pass
+
+    def get_all_vacancies(self):
+        """
+        Получает список всех вакансий
+        с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию
+        :return:
+        """
+        pass
+
+    def get_avg_salary(self):
+        """
+        Получает среднюю зарплату по вакансиям
+        :return:
+        """
